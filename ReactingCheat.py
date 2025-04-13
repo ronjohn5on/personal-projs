4fef0345270c760eb00c79d014dd2684baf12cd6
Ronjohnson <phangweijian@gmail.com>
2025-04-13 14:15:02 +0800

    first commit

---
+++ ReactingCheat.py
@@ -0,0 +1,24 @@
+# This is a script to cheat the reaction time on human calculator
+# made to learn pyautogui
+
+import pyautogui
+
+# pos = pyautogui.position()
+# print(pos)
+# im = pyautogui.screenshot()
+# print("color: "+str(im.getpixel(pos)))
+
+green = (75, 219, 106)
+blue = (43, 135, 209)
+
+while True:
+    pos = pyautogui.position()
+    # print(pos)
+    im = pyautogui.screenshot()
+    print("color: "+str(im.getpixel(pos)))
+    if im.getpixel(pos) == green:
+        pyautogui.click(pos)
+        break
+    elif im.getpixel(pos) == blue:
+        pyautogui.click(pos)
+        
